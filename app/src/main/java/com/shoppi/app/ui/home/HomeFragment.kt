bf3e4c097e1e5aec480c294772741680e3c39ccd
Anwooseong <aws5624@naver.com>
2022-11-11 05:41:40 +0900

    Data binding 과 exceptioon 해결

--- app/src/main/java/com/shoppi/app/ui/home/HomeFragment.kt
+++ app/src/main/java/com/shoppi/app/ui/home/HomeFragment.kt
@@ -12,53 +12,56 @@ import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import com.shoppi.app.*
+import com.shoppi.app.databinding.FragmentHomeBinding
 import com.shoppi.app.ui.common.ViewModelFactory
 
 class HomeFragment : Fragment() {
 
-    private val viewModel: HomeViewModel by viewModels {
-        ViewModelFactory(requireContext())
-    }
+    private val viewModel: HomeViewModel by viewModels { ViewModelFactory(requireContext()) }
+    private lateinit var binding: FragmentHomeBinding
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val toolbarTitle = view.findViewById<TextView>(R.id.toolbar_home_title)
-        val toolbarIcon = view.findViewById<ImageView>(R.id.toolbar_home_icon)
-        val viewpager = view.findViewById<ViewPager2>(R.id.viewpager_home_banner)
-        val viewpagerIndicator = view.findViewById<TabLayout>(R.id.viewpager_home_banner_indicator)
-
+        binding.lifecycleOwner = viewLifecycleOwner  //필수적임
         //viewLifecycleOwner 는 lifecycle 이 변경되는 알림을 받아 현재 lifecycle 상태를 알고 있는 객체를 의미
-        viewModel.title.observe(viewLifecycleOwner) { title->
-            toolbarTitle.text = title.text
-            GlideApp.with(this).load(title.iconUrl).into(toolbarIcon)
-        }
-
-        viewpager.adapter = HomeBannerAdapter().apply {
-            viewModel.topBanners.observe(viewLifecycleOwner) { banners->
-                submitList(banners)
-            }
-        }
-
+        setToolbar()
+        setTopBanners()
 
         val pageWidth = resources.getDimension(R.dimen.viewpager_item_width)
         val pageMargin = resources.getDimension(R.dimen.viewpage_item_margin)
         val screenWidth = resources.displayMetrics.widthPixels  //화면 가로 길이
         val offset = screenWidth - pageWidth - pageMargin
 
-        viewpager.offscreenPageLimit = 3
-        viewpager.setPageTransformer { page, position ->
+        binding.viewpagerHomeBanner.offscreenPageLimit = 3
+        binding.viewpagerHomeBanner.setPageTransformer { page, position ->
             page.translationX = position * (-offset)
         }
-        TabLayoutMediator(viewpagerIndicator, viewpager) { tab, position ->
+        TabLayoutMediator(binding.viewpagerHomeBannerIndicator, binding.viewpagerHomeBanner) { tab, position ->
 
         }.attach()
+    }
+
+    private fun setToolbar() {
+        viewModel.title.observe(viewLifecycleOwner) { title ->
+            binding.title = title
+        }
+    }
+
+    private fun setTopBanners() {
+        binding.viewpagerHomeBanner.adapter = HomeBannerAdapter().apply {
+            viewModel.topBanners.observe(viewLifecycleOwner) { banners ->
+                submitList(banners)
+            }
+        }
     }
 }
